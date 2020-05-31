2f5a4816f609e77acad3ad93c5cc646f9bd9499b
Kirill Bubochkin <ookami.kb@gmail.com>
2020-05-31 22:13:52 +0200

    :tv: Add docs (#8)

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,86 +2,57 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_arc_text/flutter_arc_text.dart';
+import 'package:storybook_flutter/storybook_flutter.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  double _startAngle = -pi / 2;
-  bool _outside = true;
-  bool _clockwise = true;
-  double _radius = 120;
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) => MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
         home: Scaffold(
-          body: SafeArea(
-            child: Column(
-              children: <Widget>[
-                Expanded(
-                  child: Center(
-                    child: Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(),
-                        borderRadius: BorderRadius.circular(_radius),
-                        color: Colors.white,
-                      ),
-                      width: _radius * 2,
-                      height: _radius * 2,
-                      child: ArcText(
-                        radius: _radius,
-                        text: 'Hello, Flutter! '
-                            'I am ArcText widget. I can draw circular text.',
-                        textStyle: TextStyle(fontSize: 18, color: Colors.black),
-                        startAngle: _startAngle,
-                        startAngleAlignment: StartAngleAlignment.start,
-                        placement:
-                            _outside ? Placement.outside : Placement.inside,
-                        direction: _clockwise
-                            ? Direction.clockwise
-                            : Direction.counterClockwise,
-                      ),
-                    ),
-                  ),
+          body: Story(
+            name: 'Arc text',
+            builder: (_, k) {
+              final radius =
+                  double.tryParse(k.text('Radius', initial: '100')) ?? 100;
+              final startAngleText = k.text('Start angle', initial: '0');
+              final startAngle =
+                  (double.tryParse(startAngleText) ?? 0) * pi / 180;
+              final text = k.text(
+                'Text',
+                initial: 'Hello, Flutter! I am ArcText widget. '
+                    'I can draw circular text.',
+              );
+              final alignment = k.options('Alignment',
+                  options: [
+                    Option('Start', StartAngleAlignment.start),
+                    Option('Center', StartAngleAlignment.center),
+                    Option('End', StartAngleAlignment.end),
+                  ],
+                  initial: StartAngleAlignment.start);
+              return Container(
+                decoration: BoxDecoration(
+                  border: Border.all(),
+                  borderRadius: BorderRadius.circular(radius),
+                  color: Colors.white,
                 ),
-                ListTile(
-                  subtitle: Slider(
-                    value: _radius,
-                    min: 90,
-                    max: 150,
-                    onChanged: (v) => setState(() => _radius = v),
-                  ),
-                  title: Text('Radius'),
+                width: radius * 2,
+                height: radius * 2,
+                child: ArcText(
+                  radius: radius,
+                  text: text,
+                  textStyle: TextStyle(fontSize: 18, color: Colors.black),
+                  startAngle: startAngle,
+                  startAngleAlignment: alignment,
+                  placement: k.boolean('Outside', initial: true)
+                      ? Placement.outside
+                      : Placement.inside,
+                  direction: k.boolean('Clockwise', initial: true)
+                      ? Direction.clockwise
+                      : Direction.counterClockwise,
                 ),
-                ListTile(
-                  subtitle: Slider(
-                    value: _startAngle,
-                    min: -pi,
-                    max: pi,
-                    onChanged: (v) => setState(() => _startAngle = v),
-                  ),
-                  title: Text('Start angle'),
-                ),
-                CheckboxListTile(
-                  value: _outside,
-                  onChanged: (v) => setState(() => _outside = v),
-                  title: Text('Outside'),
-                ),
-                CheckboxListTile(
-                  value: _clockwise,
-                  onChanged: (v) => setState(() => _clockwise = v),
-                  title: Text('Clockwise'),
-                ),
-              ],
-            ),
+              );
+            },
           ),
         ),
       );
